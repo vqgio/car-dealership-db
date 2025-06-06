ded17a6e769ff1b391a4c25aaf0b836b265d6d7d
Gio <giovannizavala82@gmail.com>
2025-06-06 14:17:45 -0500

    got table for vehicle and dealership and inserted some values in them.

--- car-dealership-database/car-dealership.sql
+++ car-dealership-database/car-dealership.sql
@@ -8,4 +8,19 @@ CREATE TABLE dealerships (
     address VARCHAR(50),
     phone VARCHAR (12)
 );
-INSERT INTO dealerships (name, address, phone) VALUES('Austin Auto', '123 Joihn St, Austin TX', '512-876-6357');
+INSERT INTO dealerships (name, address, phone)
+VALUES('Austin Auto', '123 Joihn St, Austin TX', '512-876-6357');
+INSERT INTO dealerships (name, address, phone)
+VALUES('Daves Bykes', '565 Boat Street', '637-736-8989');
+
+CREATE TABLE vehicles (
+    VIN VARCHAR(5) PRIMARY KEY,
+    make VARCHAR(30),
+    model VARCHAR(30),
+    year INT, price DECIMAL(10,2),
+    SOLD VARCHAR(9)
+)
+INSERT INTO vehicles(vin, make, model, year, price, sold)
+VALUES('12345', 'INFINITI', 'G35', 2010, 8999.99, 'AVAILABLE');
+INSERT INTO vehicles(vin, make, model, year, price, sold)
+VALUES('67663', 'DAVEMOTORS', 'DAVEMOBILE', 2003, 2300.00, 'SOLD');
